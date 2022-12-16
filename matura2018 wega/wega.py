8ac847b44b2274da82a5b7631c623bd0839aca5c
kubolbubolx <91119486+kubolbubolx@users.noreply.github.com>
2022-12-16 13:07:24 +0100

    Update wega.py

--- matura2018 wega/wega.py
+++ matura2018 wega/wega.py
@@ -25,89 +25,25 @@ def zad2(file):
 
 
 def zad3(file):
+    words = ''
     for line in file:
         line = line.strip()
-        list = []
-        counter = 0
-        dict = {}
+        b = True
         for i in line:
-            list.append(ord(i))
-        for i in list:
-            dict[i] = 0
-        for i in range(len(list) - 1):
-            if list[i] < list[i + 1]:
-                if list[i + 1] - list[i] >= 10:
-                    pass
-                else:
-                    counter += 1
-            if list[i] > list[i+1]:
-                if list[i] - list[i + 1] >= 10:
-                    pass
-                else:
-                    counter += 1
-        if len(dict) - 1 == counter:
-            filew.write(line + '\n')
-
-
-file = open('sygnaly.txt', 'r')
-file2 = open('przyklad.txt', 'r')
-filew = open('wyniki', 'w')
-
-print(zad3(file))
-
-file.close()
-file2.close()
-filew.close()
-def zad1(file):
-    counter = 0
-    wynik = ''
-    for line in file:
-        line = line.strip()
-        counter += 1
-        if counter % 40 == 0:
-            wynik += line[9]
-    filew.write(wynik)
-    return wynik
-
-
-def zad2(file):
-    max = 0
-    word = ''
-    for line in file:
-        line = line.strip()
-        dict = {}
-        for i in line:
-            dict[i] = 0
-        if len(dict) > max:
-            max = len(dict)
-            word = line
-    filew.write(word + ' ' + str(max))
-
-
-def pary(slowo):
-    if len(slowo) == 2:
-        if abs(ord(slowo[0]) - ord(slowo[1])) > 10:
-            return False
-        return True
-    for i in range(len(slowo) - 1):
-        for j in range(i + 1, len(slowo) - 1):
-            if abs(ord(slowo[i]) - ord(slowo[j])) > 10:
-                return False
-    return True
-
-
-def zad3(file):
-    for line in file:
-        line = line.strip()
-        if pary(line):
-            filew.write(line + '\n')
-
-
-
-
-
-
-
+            if line == '\n':
+                continue
+            if b:
+                for i2 in line[1:]:
+                    if i2 == '\n':
+                        continue
+                    diff = abs(ord(i) - ord(i2))
+                    if diff > 10:
+                        b = False
+            else:
+                break
+        if b:
+            words = words + line + '\n'
+    filew.write(words)
 
 
 file = open('sygnaly.txt', 'r')
