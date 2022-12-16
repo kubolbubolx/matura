fb88c05cf14afbeac0a49e2a8d68bb05cd8e3f50
kubolbubolx <91119486+kubolbubolx@users.noreply.github.com>
2022-12-16 08:50:12 +0100

    Update wega.py

--- matura2018 wega/wega.py
+++ matura2018 wega/wega.py
@@ -58,3 +58,64 @@ print(zad3(file))
 file.close()
 file2.close()
 filew.close()
+def zad1(file):
+    counter = 0
+    wynik = ''
+    for line in file:
+        line = line.strip()
+        counter += 1
+        if counter % 40 == 0:
+            wynik += line[9]
+    filew.write(wynik)
+    return wynik
+
+
+def zad2(file):
+    max = 0
+    word = ''
+    for line in file:
+        line = line.strip()
+        dict = {}
+        for i in line:
+            dict[i] = 0
+        if len(dict) > max:
+            max = len(dict)
+            word = line
+    filew.write(word + ' ' + str(max))
+
+
+def pary(slowo):
+    if len(slowo) == 2:
+        if abs(ord(slowo[0]) - ord(slowo[1])) > 10:
+            return False
+        return True
+    for i in range(len(slowo) - 1):
+        for j in range(i + 1, len(slowo) - 1):
+            if abs(ord(slowo[i]) - ord(slowo[j])) > 10:
+                return False
+    return True
+
+
+def zad3(file):
+    for line in file:
+        line = line.strip()
+        if pary(line):
+            filew.write(line + '\n')
+
+
+
+
+
+
+
+
+
+file = open('sygnaly.txt', 'r')
+file2 = open('przyklad.txt', 'r')
+filew = open('wyniki', 'w')
+
+print(zad3(file))
+
+file.close()
+file2.close()
+filew.close()
